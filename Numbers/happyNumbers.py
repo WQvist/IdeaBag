bc252bd1f0a4df653a54b5a4a73c396e9c5d2e63
William Öqvist <williamoqvist@hotmail.com>
2017-10-06 08:50:12 +0200

    Happy numbers algorithm complete

--- Numbers/happyNumbers.py
+++ Numbers/happyNumbers.py
@@ -8,3 +8,22 @@ those that do not end in 1 are unhappy numbers.
 Find the first 8 happy numbers.
 '''
 
+numToCheck = 0
+numOfHappys = 0
+while numOfHappys < 8:	# Find the first 8 happy numbers
+	numToCheck += 1
+	numString = str(numToCheck)	# Make the number into a string
+	summary = 0
+	count = 0
+	square = 0
+	while int(numString) != 1 and count < 5000:	# Assume that no happy number requires more than 5000 loops
+		for y in range(len(numString)):
+			summary = summary + int(numString[y])**2	# Square the current number and add it to summary
+			count+=1
+			if y+1 == len(numString):		# If all "chars" in the number have been checked
+				numString = str(summary)	# Start checking the new number
+				summary = 0
+	if int(numString) == 1:
+		print(str(numToCheck) + " is a happy number")
+		numOfHappys+=1
+	
